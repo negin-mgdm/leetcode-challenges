eb52f26092447d28aa0dc62944a2119cc1c90607
negin-mgdm <negin.mgdm@gmail.com>
2024-04-13 01:15:23 +0100

    refactor: Creating a dictionary to store the count of each character, to prevent "Time Limit Exceeded" errors.

--- FirstUniqueCharacter.py
+++ FirstUniqueCharacter.py
@@ -20,19 +20,49 @@ s consists of only lowercase English letters.
 '''
 
 
+# class Solution(object):
+#     def firstUniqChar(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         sentence = len(s.lower())
+#         for i in range(sentence):
+#             if sentence.count(sentence[i]) == 1:
+#                 return i
+#         return -1
+
+
+# s = "LeetCode"
+# char_index = Solution().firstUniqChar(s)
+# print(char_index)
+
+
+# Below is the optimised code to avoid "Time Limit Exceeded" errors:
+
 class Solution(object):
     def firstUniqChar(self, s):
         """
         :type s: str
         :rtype: int
         """
-        sentence = s.lower()
-        for i in range(len(sentence)):
-            if sentence.count(sentence[i]) == 1:
+        # Create a dictionary to store the count of each character.
+        char_count = {}
+
+        # Count the frequency of each character in the string.
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+
+        # Find the index of the first unique character.
+        for i, char in enumerate(s):
+            if char_count[char] == 1:
                 return i
+
+        # If no unique character is found, return -1.
         return -1
 
 
 s = "LeetCode"
-char_index = Solution().firstUniqChar(s)
+solution = Solution()
+char_index = solution.firstUniqChar(s)
 print(char_index)
