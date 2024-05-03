cbb1529f11b6126a4b48de539dce5c4bc33e220b
negin-mgdm <negin.mgdm@gmail.com>
2024-05-03 01:14:04 +0100

    feat: Implementing a method to determine if each child can have the greatest number of candies by adding extra candies to their current count. The method iterates through the candy counts, checks if adding the extra candies allows the child to have the maximum number of candies, and returns a list of boolean values accordingly.

--- Easy Problems/Kids_With_the_Greatest_Number_of_Candies.py
+++ Easy Problems/Kids_With_the_Greatest_Number_of_Candies.py
@@ -35,3 +35,17 @@ class Solution(object):
         :type extraCandies: int
         :rtype: List[bool]
         """
+        max_candies = max(candies)
+        result = []
+        for candy in candies:
+            if (candy + extraCandies) >= max_candies:
+                result.append(True)
+            else:
+                result.append(False)
+        return result
+
+
+candies = [2, 3, 5, 1, 3]
+extraCandies = 3
+solution = Solution()
+print(solution.kidsWithCandies(candies, extraCandies))
