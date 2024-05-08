c655a2642961b2aded8a4861477556b2ff1a2676
negin-mgdm <negin.mgdm@gmail.com>
2024-05-08 02:36:09 +0100

    feat: Implementing a method to move zeroes to the end of the list by iterating through the list, deleting zero elements and appending them to the end, while adjusting the length of the list accordingly.

--- Easy Problems/MoveZeroes.py
+++ Easy Problems/MoveZeroes.py
@@ -19,3 +19,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        i = 0
+        length = len(nums)
+        while i < length:
+            if nums[i] == 0:
+                del nums[i]
+                nums.append(0)
+                length -= 1     # Decrease the length since we've moved an element to the end
+            else:
+                # Move to the next element in the list by incrementing the index i by 1 (i = i + 1).
+                i += 1
+
+
+nums = [0, 1, 0, 0, 1, 0]
+solution = Solution()
+print(solution.moveZeroes(nums))
