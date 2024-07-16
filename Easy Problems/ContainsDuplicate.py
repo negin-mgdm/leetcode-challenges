302397723d20c707ab702443e1efb8f1941e6a07
negin-mgdm <negin.mgdm@gmail.com>
2024-07-16 19:53:01 +0100

    feat: Implementing method to determine if a list contains duplicate elements by using a dictionary.

---
+++ Easy Problems/ContainsDuplicate.py
@@ -0,0 +1,45 @@
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+'''
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count = {}
+
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        for key in count:
+            if count[key] > 1:
+                return True
+        return False
+
+
+nums = [1, 2, 3, 1]
+solution = Solution()
+print(solution.containsDuplicate(nums))
