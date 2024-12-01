6509a0f3ef9b4d96d6433b2980dfa39574e19bbc
negin-mgdm <negin.mgdm@gmail.com>
2024-12-01 22:38:49 +0000

    feat: Implementing function to verify if each digit in a string matches its count as described by its index.

---
+++ Easy Problems/Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value.js
@@ -0,0 +1,53 @@
+/*
+You are given a 0-indexed string num of length n consisting of digits.
+Return true if for every index i in the range 0 <= i < n, the digit i occurs num[i] times in num, otherwise return false.
+
+Example 1:
+Input: num = "1210"
+Output: true
+Explanation:
+num[0] = '1'. The digit 0 occurs once in num.
+num[1] = '2'. The digit 1 occurs twice in num.
+num[2] = '1'. The digit 2 occurs once in num.
+num[3] = '0'. The digit 3 occurs zero times in num.
+The condition holds true for every index in "1210", so return true.
+
+Example 2:
+Input: num = "030"
+Output: false
+Explanation:
+num[0] = '0'. The digit 0 should occur zero times, but actually occurs twice in num.
+num[1] = '3'. The digit 1 should occur three times, but actually occurs zero times in num.
+num[2] = '0'. The digit 2 occurs zero times in num.
+The indices 0 and 1 both violate the condition, so return false.
+ 
+Constraints:
+n == num.length
+1 <= n <= 10
+num consists of digits.
+*/
+
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var digitCount = function (num) {
+    let numsCount = {};
+    for (let digit of num) {
+        if (digit in numsCount) {
+            numsCount[digit] += 1;
+        } else {
+            numsCount[digit] = 1;
+        }
+    }
+    for (let i = 0; i < num.length; i++) {
+        let digit = i.toString();
+        if ((numsCount[digit] || 0) !== parseInt(num[i])) {
+            return false;
+        }
+    }
+    return true;
+};
+
+let num = "1210";
+console.log(digitCount(num));
