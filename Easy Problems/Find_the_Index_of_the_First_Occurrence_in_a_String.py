5041976a8e0a84d472680b8243e4e744b32c60e2
negin-mgdm <negin.mgdm@gmail.com>
2024-12-15 20:56:34 +0000

    feat: Implementing method to find the first occurrence of a substring in a string.

---
+++ Easy Problems/Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -0,0 +1,39 @@
+'''
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+Constraints:
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+'''
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            index = haystack.index(needle)
+        else:
+            index = -1
+        return index
+
+
+haystack = "leetcode"
+needle = "leeto"
+solution = Solution()
+print(solution.strStr(haystack, needle))
