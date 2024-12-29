448322659f36eafe8d6255a66117f4d2306db60c
negin-mgdm <negin.mgdm@gmail.com>
2024-12-29 19:00:14 +0000

    feat: Implementing function to count good triplets based on given conditions.

---
+++ Easy Problems/Count_Good_Triplets.js
@@ -0,0 +1,56 @@
+/*
+Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+0 <= i < j < k < arr.length
+|arr[i] - arr[j]| <= a
+|arr[j] - arr[k]| <= b
+|arr[i] - arr[k]| <= c
+Where |x| denotes the absolute value of x.
+Return the number of good triplets.
+
+Example 1:
+Input: arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+Output: 4
+Explanation: There are 4 good triplets: [(3,0,1), (3,0,1), (3,1,1), (0,1,1)].
+
+Example 2:
+Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+Output: 0
+Explanation: No triplet satisfies all conditions.
+ 
+Constraints:
+3 <= arr.length <= 100
+0 <= arr[i] <= 1000
+0 <= a, b, c <= 1000
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function (arr, a, b, c) {
+    let triplets = [];
+    let n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            for (let k = j + 1; k < n; k++) {
+                if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                    let triplet = (arr[i], arr[j], arr[k]);
+                    triplets.push(triplet);
+                }
+            }
+        }
+    }
+
+    return triplets.length;
+};
+
+let arr = [3, 0, 1, 1, 9, 7];
+let a = 7;
+let b = 2;
+let c = 3;
+console.log(countGoodTriplets(arr, a, b, c));
