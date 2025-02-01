9859aa8b547995c07a071abb3a66830a6b192115
negin-mgdm <negin.mgdm@gmail.com>
2025-02-01 16:03:16 +0000

    refactor: Renaming variables for better readability.

--- Easy Problems/Decrypt_String_from_Alphabet_to_Integer_Mapping.py
+++ Easy Problems/Decrypt_String_from_Alphabet_to_Integer_Mapping.py
@@ -27,27 +27,27 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        ascii_mapping_reversed = {
+        char_mapping = {
             '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f', '7': 'g', '8': 'h', '9': 'i',
             '10#': 'j', '11#': 'k', '12#': 'l', '13#': 'm', '14#': 'n', '15#': 'o', '16#': 'p', '17#': 'q',
             '18#': 'r', '19#': 's', '20#': 't', '21#': 'u', '22#': 'v', '23#': 'w', '24#': 'x', '25#': 'y', '26#': 'z'
         }
 
-        alpha = ""
+        decrypted_string = ""
         i = 0
 
         while i < len(s):
 
             if i+2 < len(s) and s[i+2] == "#":
-                x = s[i:i+2]+"#"
-                alpha += ascii_mapping_reversed[x]
+                two_digit_key = s[i:i+2]+"#"
+                decrypted_string += char_mapping[two_digit_key]
                 i += 3
 
             else:
-                alpha += ascii_mapping_reversed[s[i]]
+                decrypted_string += char_mapping[s[i]]
                 i += 1
 
-        return alpha
+        return decrypted_string
 
 
 s = "10#11#12"
