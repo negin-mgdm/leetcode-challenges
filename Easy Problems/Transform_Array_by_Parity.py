22e96fe20b26de8c958ce5196cb386e2ae3c611a
Negin Moghadam <negin.mgdm@gmail.com>
2025-03-21 22:29:59 +0000

    feat: Implementing method to transform array by replacing even/odd numbers.

---
+++ Easy Problems/Transform_Array_by_Parity.py
@@ -0,0 +1,49 @@
+'''
+You are given an integer array nums. Transform nums by performing the following operations in the exact order specified:
+Replace each even number with 0.
+Replace each odd numbers with 1.
+Sort the modified array in non-decreasing order.
+Return the resulting array after performing these operations.
+
+Example 1:
+Input: nums = [4,3,2,1]
+Output: [0,0,1,1]
+Explanation:
+Replace the even numbers (4 and 2) with 0 and the odd numbers (3 and 1) with 1. Now, nums = [0, 1, 0, 1].
+After sorting nums in non-descending order, nums = [0, 0, 1, 1].
+
+Example 2:
+Input: nums = [1,5,1,4,2]
+Output: [0,0,1,1,1]
+Explanation:
+Replace the even numbers (4 and 2) with 0 and the odd numbers (1, 5 and 1) with 1. Now, nums = [1, 1, 1, 0, 0].
+After sorting nums in non-descending order, nums = [0, 0, 1, 1, 1].
+ 
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 1000
+'''
+
+
+class Solution(object):
+    def transformArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        new_arr = []
+
+        for num in nums:
+            if num % 2 == 0:
+                new_arr.append(0)
+            elif num % 2 != 0:
+                new_arr.append(1)
+            else:
+                new_arr.append(num)
+
+        return sorted(new_arr)
+
+
+nums = [4, 3, 2, 1]
+solution = Solution()
+print(solution.transformArray(nums))
