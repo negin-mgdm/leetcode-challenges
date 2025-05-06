11981af6cea04f1ba1d2a82edc912888ffabe992
Negin Moghadam <negin.mgdm@gmail.com>
2025-05-06 10:56:53 +0100

    refactor: Simplifying temperature conversion usage in main execution.

--- Easy Problems/Convert_the_Temperature.py
+++ Easy Problems/Convert_the_Temperature.py
@@ -1,21 +1,36 @@
-# You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+'''
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+Note that:
+Kelvin = Celsius + 273.15
+Fahrenheit = Celsius * 1.80 + 32.00
+ 
+Example 1:
+Input: celsius = 36.50
+Output: [309.65000,97.70000]
+Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
 
-# You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+Example 2:
+Input: celsius = 122.11
+Output: [395.26000,251.79800]
+Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+ 
+Constraints:
+0 <= celsius <= 1000
+'''
 
-# Return the array ans. Answers within 10-5 of the actual answer will be accepted.
 
 class Solution:
     def convertTemperature(self, celsius):
         temp = []
+
         temp.append(celsius + 273.15)
         temp.append(celsius * 1.80 + 32.00)
 
         return temp
 
 
-celsius = Solution()
-degree = celsius.convertTemperature(10.56)
-
-degree_formatted = list(map(lambda x: '{:.5f}'.format(x), degree))
-
-print(degree_formatted)
+celsius = 36.50
+solution = Solution()
+print(solution.convertTemperature(celsius))
