acf5df9b01cef2004b38d4ae2ee72702ac92f1d5
Negin Moghadam <negin.mgdm@gmail.com>
2025-06-10 22:30:15 +0100

    feat: Adding logic for finding customers who never order.

---
+++ Easy Problems/Customers_Who_Never_Order.py
@@ -0,0 +1,71 @@
+'''
+Table: Customers
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+
+Table: Orders
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+Write a solution to find all customers who never order anything.
+Return the result table in any order.
+The result format is in the following example.
+
+Example 1:
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+'''
+
+import pandas as pd
+
+
+data = [[1, 'Joe'], [2, 'Henry'], [3, 'Sam'], [4, 'Max']]
+customers = pd.DataFrame(data, columns=['id', 'name']).astype(
+    {'id': 'Int64', 'name': 'object'})
+data = [[1, 3], [2, 1]]
+orders = pd.DataFrame(data, columns=['id', 'customerId']).astype(
+    {'id': 'Int64', 'customerId': 'Int64'})
+
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    customers_without_orders = customers[~customers['id'].isin(
+        orders['customerId'])]
+    return customers_without_orders[['name']].rename(columns={'name': 'Customers'})
+
+
+print(find_customers(customers, orders))
