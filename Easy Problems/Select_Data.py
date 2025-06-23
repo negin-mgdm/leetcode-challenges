40cb5522ce6ab9a5a8551540ff647d8e02a10403
Negin Moghadam <negin.mgdm@gmail.com>
2025-06-23 15:48:08 +0100

    feat: Adding function to select name and age of student with ID 101.

---
+++ Easy Problems/Select_Data.py
@@ -0,0 +1,47 @@
+'''
+DataFrame students
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
++-------------+--------+
+Write a solution to select the name and age of the student with student_id = 101.
+The result format is in the following example.
+
+Example 1:
+Input:
++------------+---------+-----+
+| student_id | name    | age |
++------------+---------+-----+
+| 101        | Ulysses | 13  |
+| 53         | William | 10  |
+| 128        | Henry   | 6   |
+| 3          | Henry   | 11  |
++------------+---------+-----+
+Output:
++---------+-----+
+| name    | age |
++---------+-----+
+| Ulysses | 13  |
++---------+-----+
+Explanation:
+Student Ulysses has student_id = 101, we select the name and age.
+'''
+import pandas as pd
+
+data = {
+    'student_id': [101, 53, 128, 3],
+    'name': ['Ulysses', 'William', 'Henry', 'Henry'],
+    'age': [13, 10, 6, 11]
+}
+
+students = pd.DataFrame(data)
+
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students['student_id'] == 101, ['name', 'age']]
+
+
+print(selectData(students))
