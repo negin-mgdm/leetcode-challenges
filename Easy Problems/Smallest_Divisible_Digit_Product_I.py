76b105236e3f258ec71ddc7ff900f13ffb222ee4
Negin Moghadam <negin.mgdm@gmail.com>
2025-07-29 23:22:48 +0100

    feat: Implementing method to find the smallest number with digit product divisible by t.

---
+++ Easy Problems/Smallest_Divisible_Digit_Product_I.py
@@ -0,0 +1,42 @@
+'''
+You are given two integers n and t.
+Return the smallest number greater than or equal to n such that the product of its digits is divisible by t.
+
+Example 1:
+Input: n = 10, t = 2
+Output: 10
+Explanation:
+The digit product of 10 is 0, which is divisible by 2, making it the smallest number greater than or equal to 10 that satisfies the condition.
+
+Example 2:
+Input: n = 15, t = 3
+Output: 16
+Explanation:
+The digit product of 16 is 6, which is divisible by 3, making it the smallest number greater than or equal to 15 that satisfies the condition.
+
+Constraints:
+1 <= n <= 100
+1 <= t <= 10
+'''
+
+
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        def product_calculator(n):
+            product = 1
+            for digit in str(n):
+                product *= int(digit)
+            return product
+
+        while True:
+            result = product_calculator(n)
+            if result % t == 0:
+                return n
+            else:
+                n += 1
+
+
+n = 15
+t = 3
+solution = Solution()
+print(solution.smallestNumber(n, t))
