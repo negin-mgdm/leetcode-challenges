3850d4e31d8845290fda0176043573f2d4f812e0
Negin Moghadam <negin.mgdm@gmail.com>
2025-09-11 20:37:46 +0100

    feat: Implementing method that groups numbers from 1 to n by the sum of their digits and returns the number of groups that have the maximum size.

---
+++ Easy Problems/Count_Largest_Group.py
@@ -0,0 +1,51 @@
+'''
+You are given an integer n.
+We need to group the numbers from 1 to n according to the sum of its digits. 
+For example, the numbers 14 and 5 belong to the same group, whereas 13 and 3 belong to different groups.
+Return the number of groups that have the largest size, i.e. the maximum number of elements.
+
+Example 1:
+Input: n = 13
+Output: 4
+Explanation: There are 9 groups in total, they are grouped according sum of its digits of numbers from 1 to 13:
+[1,10], [2,11], [3,12], [4,13], [5], [6], [7], [8], [9].
+There are 4 groups with largest size.
+
+Example 2:
+Input: n = 2
+Output: 2
+Explanation: There are 2 groups [1], [2] of size 1.
+ 
+Constraints:
+1 <= n <= 104
+'''
+
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        groups = {}
+
+        for num in range(1, n + 1):
+            digit_sum = 0
+            temp = num
+            while temp > 0:
+                digit_sum += temp % 10
+                temp //= 10
+
+            if digit_sum not in groups:
+                groups[digit_sum] = 0
+            groups[digit_sum] += 1
+
+        max_size = max(groups.values())
+
+        count = 0
+        for size in groups.values():
+            if size == max_size:
+                count += 1
+
+        return count
+
+
+n = 13
+solution = Solution()
+print(solution.countLargestGroup(n))
